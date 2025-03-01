58deca1ca7aa92dc6d8f8fabedab86f4fe375ba5
Ben Chi <benthechi@gmail.com>
2025-02-28 21:50:55 -0800

    Dynamic path

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { useState, useEffect } from "react";
 import { io, Socket } from "socket.io-client";
 
+//PROD
+const API_URL = "/api";
+const PATH = { path: "/api/socket.io" };
+
+//DEV
+// const API_URL = "http://localhost:4000";
+// const PATH = {};
+
 function App() {
   const [socket, setSocket] = useState<Socket | null>(null);
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState<string[]>([]);
 
   useEffect(() => {
-    const newSocket = io("http://localhost:4000"); //Local Dev backend route
+    const newSocket = io(API_URL, PATH); //Local Dev backend route
     setSocket(newSocket);
 
     newSocket.on("chat-message", (message) => {
