22d6f20cc3a8b6b1fe0864a7d23c98d2c02c93f7
Saif Lakhani <saiflakhani@gmail.com>
2025-03-01 16:50:34 -0800

    added Imani's changes in Lobby

--- src/components/Lobby.tsx
+++ src/components/Lobby.tsx
@@ -1,6 +1,4 @@
 import { GameObject } from "../model/game_object";
-import { sendMessage } from "../socket_handler/message_handler";
-import { USER_READY_KEY } from "../constants/socket_keys";
 import { GameState } from "../model/game_state";
 
 interface LobbyProps {
@@ -14,11 +12,130 @@ export default function Lobby({ gameObject, setGameState }: LobbyProps) {
       <h1>Lobby</h1>
       {gameObject ? (
         <>
-          <p>Users: {gameObject.users.map((user) => user.name).join(", ")}</p>
-          <p>Theme: </p>
-          <button onClick={() => sendMessage(USER_READY_KEY, "true")}>
-            Start Game
-          </button>
+          <body style={{display: "flex", 
+    flexDirection: "row"}}>
+        {/* Setting of the body flexbox */}
+
+                <div className="lobbyMain" style={{display: "flex", 
+                    flexDirection: "column", }}>
+                        {/* Outermost Left flex box creation & nested flex boxes */}
+
+                    <div className="gameConfig" style={{ display: "flex", 
+                        flexDirection: "row" }}>
+                            {/* Container for the yellow Theme & World setting flex boxes */}
+
+                        <div className="themeSection" style={{display: "flex", 
+                            flexDirection: "column"}}>
+
+                            <h1>Theme</h1>
+
+                            <div className="themeBttns" style={{display: "flex", 
+                                flexDirection:"column", 
+                                justifyContent: "space-evenly"}}>
+
+                                <button className="themeOption">
+                                    Adventure
+                                </button>
+
+                                <button className="themeOption">
+                                    Action
+                                </button>
+
+                                <button className="themeOption">
+                                    Romance
+                                </button>
+
+                                <button className="themeOption">
+                                    Mystery
+                                </button>
+                            </div>
+                            {/*End Theme button selection Section */}
+
+                        </div>
+                                {/* End Theme Side */}
+
+                        <div className="settingSection" style={{display:"flex", 
+                            flexDirection: "column",}}>
+
+                            <h1>World Setting</h1>
+
+                            <div className="settingBttns" style={{display:"flex", flexDirection:"column"}}>
+
+                                <button className="settingOption">
+                                    Medieval
+                                </button>
+
+                                <button className="settingOption">
+                                    Fantasy
+                                </button>
+
+                                <button className="settingOption">
+                                    Sci-Fi
+                                </button>
+
+                                <button className="settingOption">
+                                    Horror
+                                </button>
+                                {/*End World Setting button selection Section */}
+
+                            </div>
+                        </div>
+                        {/*End Setting side */}
+
+                    </div>
+                    {/*End Yellow highlighted flexbox container */}
+
+                    <div className="turns">
+                        <h3>No. of Turns</h3>
+
+                    </div>
+                    {/*End Turns dropdown */}
+
+                    <div className="bttnSection">
+                        <button className="startBttn" style={{fontWeight:"bolder", padding: "30px"}}>
+                            START
+                        </button>
+                    </div>
+                    {/*End Start button */}
+
+                </div>
+                {/* End of Outer most, left-side flex box container (large) */}
+
+                <div className="playerInfo" style={{display:"flex", 
+                    flexDirection:"column",}}>
+                        {/*Creation of Outermost, right-side flex box container (large/skinny) */}
+
+                    <div className="timer">
+                        <h3>Timer</h3>
+
+                    </div>
+                    {/*End Timer Section */}
+                
+                    <div className="playerList" style={{display:"flex", 
+                        flexDirection: "column"}}>
+                        
+                        <h3>Players List</h3>
+                        <ul>
+                        {gameObject.users.map((user) => (
+                            <li>
+                                {user.name} {user.isHost ? "(Host)" : ""}
+                            </li>
+                        ))}
+                        </ul>
+
+                    </div>
+                    {/*End Player list Section */}
+
+                    <div className="chat">
+                        <h3>Chat Box</h3>
+
+                    </div>
+                    {/*End Chat box Section */}
+
+                </div>
+                {/*End of Outer most, right-side flex box container (large/skinny) */}
+
+            </body>;
         </>
       ) : (
         <p>Loading...</p>
