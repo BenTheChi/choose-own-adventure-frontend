4169abf0d4c1602f502f372615f080ce04c8df2b
Ben Chi <benthechi@gmail.com>
2025-03-03 14:14:03 -0800

    Remove unused hook

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { GameObject } from "./model/game_object";
-import { Provider, useGameContext } from "./components/Provider";
+import { Provider } from "./components/Provider";
 
 const initialGameObject: GameObject = {
   title: "",
